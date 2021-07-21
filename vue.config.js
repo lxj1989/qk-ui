2276e04b757d6db8f09c8be0218ffe0e329dcf8e
lxj_1989 <liuxianjun520@126.com>
2021-07-21 19:52:23 +0800

    2021-07-21 19:52:22

--- vue.config.js
+++ vue.config.js
@@ -18,7 +18,7 @@ module.exports = {
 	runtimeCompiler: true,
 	publicPath: '/',
 	outputDir: 'docs',
-	assetsDir: 'static',
+	assetsDir: 'docs/static',
 	lintOnSave: false, // 关闭eslint
 	productionSourceMap: false,
 	devServer: {
